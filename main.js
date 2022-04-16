52c58034c59c3bfeced3d1e5f2df9db35f0ad0ca
richez003 <richemonday@gmail.com>
2022-04-16 02:11:11 +0100

    made section to display an error message when the email is in uppercase

--- main.js
+++ main.js
@@ -113,3 +113,73 @@ openMenu3.forEach((link) => link.addEventListener('click', () => {
 closeMe2.forEach((link) => link.addEventListener('click', () => {
   popUp1.style.display = 'none';
 }));
+
+function disableScroll() {
+  // Get the current page scroll position
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  // if any scroll is attempted, set this to the previous value
+  window.onscroll = function scroller() {
+    window.scrollTo(scrollLeft, scrollTop);
+  };
+
+// const email = document.getElementById('email');
+// const form = document.getElementById('form-style');
+// const displayMsg = document.getElementById('#form-message-error');
+// displayMsg.innerHTML = 'Your email address should be in lowercase';
+// form.addEventListener('submit', (event) => {
+//   if (email.value !== email.value.toLowerCase()) {
+//     event.preventDefault();
+//     displayMsg.style.visibility = 'visible';
+//     displayMsg.classList.add('error-msg');
+//     setTimeout(() => {
+//       displayMsg.style.visibility = 'hidden';
+//     }, 3000);
+//   } else {
+//     displayMsg.style.visibility = 'hidden';
+//   }
+// });
+
+const contactForm = document.querySelector('.form-style');
+const emailInput = document.querySelector('.email');
+const validationMessage = document.querySelector('.error');
+contactForm.addEventListener('submit', (event) => {
+  const strInput = emailInput.value;
+  if (/[A-Z]/.test(strInput)) {
+    validationMessage.innerHTML = 'Your email address should not contain upper case letters';
+    validationMessage.classList.add('shake');
+    event.preventDefault();
+  }
+});
+
+const firstName = document.getElementById('name1');
+const lastName = document.getElementById('name2');
+const messageInput = document.getElementById('message');
+ 
+function checkLocalStorage() {
+    const values = localStorage.getItem('input');
+
+    let input = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: '',};
+
+    if (values) {
+      input = JSON.parse(values);
+     firstName.value = input.name1;
+      lastName.value = input.name2;
+      emailInput.value = input.email;
+      messageInput.value = input.message;
+
+    } else {
+      localStorage.setItem('input', JSON.stringify(input));
+    }
+  }
+  function updateLocalStorage(e) {
+    input = { ...input, [e.target.name]: e.target.value };
+    const serialized = JSON.stringify(input);
+    localStorage.setItem('input', serialized);
+  }
+}
